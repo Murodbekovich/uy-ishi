5265162d7473a55af75d4808d782cb88124621a3
Murodbekovich <amatovbunyodbek671@gmail.com>
2025-11-29 22:17:26 +0500

    2-dars

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json("salom")
+})
+
+app.listen(3000, () => {
+    console.log("Server ishladi");
+})
